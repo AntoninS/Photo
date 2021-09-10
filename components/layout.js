459d01aba9c94a7a16f868bc8e0fcf03e2299b81
AntoninS <antonin.spontak@gmail.com>
2021-09-10 10:02:55 +0200

    feat(APP): created layout + navbar components

---
+++ components/layout.js
@@ -0,0 +1,11 @@
+import Navbar from './navbar'
+import styles from './layout.module.css'
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <Navbar />
+      <main className={styles.main}>{children}</main>
+    </>
+  )
+}
